93ba01c8f5c2dfead64e569cbab3a3d3d7883251
Joe Geldart <joegeldart@Joes-Air.home>
2015-03-14 07:04:39 +0000

    Add ditaa and salt diagram types to UML tag support

--- lib/eyemask/liquid/uml.rb
+++ lib/eyemask/liquid/uml.rb
@@ -8,11 +8,12 @@ module Eyemask
     class Uml < ::Liquid::Block
 
       def initialize(tag_name, markup, tokens)
+        @tag_name = tag_name
         super
       end
 
       def render(context)
-        "\n\n<figure><img class=\"resolution-print\" src=\"#{get_data_uri_for_diagram(super)}\"></figure>\n\n"
+        "\n\n<figure><img class=\"resolution-#{resolution}\" src=\"#{get_data_uri_for_diagram(super)}\"></figure>\n\n"
       end
 
       def get_data_uri_for_diagram(diagram)
@@ -23,18 +24,49 @@ module Eyemask
       def parse_diagram(diagram)
         output = ""
         IO.popen(["plantuml", "-pipe"], 'r+') do |f|
-          f.puts("skinparam backgroundColor transparent")
-          f.puts("skinparam shadowing false")
-          f.puts("skinparam dpi 300")
+          f.puts("@start#{diagram_type}")
+          case diagram_type
+          when "uml"
+            f.puts("skinparam backgroundColor transparent")
+            f.puts("skinparam shadowing false")
+            f.puts("skinparam dpi 300")
+          end
           f.puts(diagram)
+          f.puts("@end#{diagram_type}")
           f.close_write
           output = f.read
         end
         output
       end
 
+      def resolution
+        case diagram_type
+        when "uml"
+          "print"
+        else
+          "normal"
+        end
+      end
+
+      def diagram_type
+        case @tag_name
+        when "uml"
+          "uml"
+        when "ditaa"
+          "ditaa"
+        when "salt"
+          "salt"
+        else
+          "uml"
+        end
+      end
+
     end
+
   end
 end
 
 Liquid::Template.register_tag('uml', Eyemask::Liquid::Uml)
+Liquid::Template.register_tag('ditaa', Eyemask::Liquid::Uml)
+Liquid::Template.register_tag('salt', Eyemask::Liquid::Uml)
+Liquid::Template.register_tag('dot', Eyemask::Liquid::Uml)
